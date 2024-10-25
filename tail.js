fd36951d0070fb7777e312a4b6deb75b74ebdea4
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-10-25 17:19:51 -0400

    Corrected the empty array result

--- tail.js
+++ tail.js
@@ -22,5 +22,4 @@ assertEqual(singleTail.length, 0);
 
 const empty = []; // Unable to figure out who to get passed instead of fail
 const emptyTail = tail(empty);
-assertEqual(emptyTail, []);
-assertEqual(empty.length, 0);
+assertEqual(emptyTail.length, 0);
