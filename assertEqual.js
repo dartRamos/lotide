fee828224147ab1994f4ccc9e1d2eccad2ebced3
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-10-25 15:08:14 -0400

    Learned how to use assertEuqal to add style with notes

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🎇🎇🎇Assertion Passed: ${actual} === ${expected}`);
+    console.log(`🎇🎇🎇Assertion Passed: ${actual} === ${expected}`); // Using `` allows you to embed expressions inside ${} in string
   } else {
     console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Bootcamp"); // assertEqual allows for more style like adding emotes
 assertEqual(1, 1);
