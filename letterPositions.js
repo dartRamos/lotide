b0a0cfca59d0f39ac971ccc8b000ad0e3ef1e8d5
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-10-30 18:19:49 -0400

    counter position of letters excluding spaces

---
+++ letterPositions.js
@@ -0,0 +1,52 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🎇🎇🎇Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  sentence = sentence.replace(/ /g, "");
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (!results[sentence[i]]) {
+      results[sentence[i]] = [i];
+    } else {
+      results[sentence[i]].push(i);
+    }
+  }
+
+    return results;
+  };
+
+const result = letterPositions("hello");
+
+assertArraysEqual(result["h"], [0]);
+assertArraysEqual(result["e"], [1]);
+assertArraysEqual(result["l"], [2, 3]);
+assertArraysEqual(result["o"], [4]);
+
+const result1 = letterPositions("hello world");
+
+assertArraysEqual(result1["h"], [0]);
+assertArraysEqual(result1["e"], [1]);
+assertArraysEqual(result1["l"], [2, 3, 8]);
+assertArraysEqual(result1["o"], [4, 6]);
+assertArraysEqual(result1["w"], [5]);
+assertArraysEqual(result1["r"], [7]);
+assertArraysEqual(result1["d"], [9]);
