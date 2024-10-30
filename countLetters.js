d82b2cf5b6fa73b4b2f371907ba1d9b9a46abd6e
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-10-30 17:30:32 -0400

    counting letters in a string

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎇🎇🎇Assertion Passed: ${actual} === ${expected}`); // Using `` allows you to embed expressions inside ${} in string
+  } else {
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (string) {
+  const result = {};
+
+  for (const letter of string) {
+    if (letter !== " ") {
+      if(result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+
+  return result;
+};
+
+const result1 = countLetters("lighthouse in the house");
+
+assertEqual(result1["l"], 2);
+assertEqual(result1["i"], 2);
+assertEqual(result1["e"], 3);
