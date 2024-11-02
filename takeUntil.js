31354acc8115eb61f0e8cfe3ba194bd0748fe70a
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-11-02 15:46:56 -0400

    added comments to better explain what the function does. must remember to do this

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,4 @@
+// The takeUntil function takes an array and a callback as arguments. It iterates through the array and returns an array of elements that appear before the one that causes the callback to return a truthy value
 const takeUntil = function(array, callback) {
   const result = [];
 
