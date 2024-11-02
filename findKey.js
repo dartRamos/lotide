5cbea06c334433d635024f63cf277686f0ff075a
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-11-02 16:14:13 -0400

    wrote code that loops through object and returns the key that has the first value of stars we are looking for. Does not show key that also has same stars if it isnt the first one found

---
+++ findKey.js
@@ -0,0 +1,38 @@
+const findKey = function (obj, callback) {
+  for (let key in obj) {
+    if(callback(obj[key])) {
+      return key;
+    }
+  }
+
+  return undefined;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎇🎇🎇Assertion Passed: ${actual} === ${expected}`); // Using `` allows you to embed expressions inside ${} in string
+  } else {
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const objGiven =
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  };
+
+
+assertEqual(findKey(objGiven, (x) => x.stars === 2), "noma");
+
+assertEqual(findKey(objGiven, (x) => x.stars === 3), "Akaleri");
+
+assertEqual(findKey(objGiven, (x) => x.stars === 4), undefined);
+
+assertEqual(findKey(objGiven, (x) => x.stars === 1), "Blue Hill");
+
+assertEqual(findKey(objGiven, (x) => x.stars === 0), undefined);
