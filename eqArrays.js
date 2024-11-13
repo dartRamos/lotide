80c4fa561eccf8e35858d4bd410199e36658daa4
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-11-13 09:56:02 -0800

    Moved eqArray to test folder. Added export of eqArray n eqArray.js code. Ran to make sure its exporting and requiring properly

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎇🎇🎇Assertion Passed: ${actual} === ${expected}`); // Using `` allows you to embed expressions inside ${} in string
-  } else {
-    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -19,7 +11,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["Bob"], ["Rob"]), false);
-assertEqual(eqArrays(["java"], ["java"]), true);
+module.exports = eqArrays;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,23 @@
 const assertEqual = require("../assertEqual");
 const tail = require("../tail")
 
+// Test case: Check the tail of an array with multiple elements
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
 
+// Test case: Check the tail of an array with a single element
 const single = ["Hello"];
 const singleTail = tail(single);
 assertEqual(singleTail.length, 0);
 
+// Test case: Check the tail of an empty array
 const empty = [];
 const emptyTail = tail(empty);
 assertEqual(emptyTail.length, 0);
 
+// Test case: Check that the original array is not modified
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
